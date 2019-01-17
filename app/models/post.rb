f3268f24cb2412086fbe0a0fb05141727a6a3f88
Christopher Tompkins <chtompkins026@gmail.com>
2019-01-17 21:19:49 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,7 +10,7 @@ class Post < ActiveRecord::Base
     where(author: author_id)
   end
   
-    def self.from_today
+  def self.from_today
     where("created_at >=?", Time.zone.today.beginning_of_day)
   end
  
